3244d00a11a7d76ef63b1c773929a1b439c46771
sumin <166582952+royalqwerdf@users.noreply.github.com>
2024-05-15 22:51:25 +0900

    Initial commit

---
+++ src/main/java/com/elice/homealone/HomealoneApplication.java
@@ -0,0 +1,13 @@
+package com.elice.homealone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomealoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomealoneApplication.class, args);
+	}
+
+}
