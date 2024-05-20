4c7de669fdb39f98a851bee7b0a969614f71deda
Lazy Cat <mylazysundaycat@gmail.com>
2024-05-20 15:19:14 +0900

    feat: Member Entity 초안

--- src/main/java/com/elice/homealone/member/entity/Member.java
+++ src/main/java/com/elice/homealone/member/entity/Member.java
@@ -1,4 +1,35 @@
 package com.elice.homealone.member.entity;
 
-public class Member {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "member")
+public class Member{
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name", length = 15, nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    //Enum 사용?
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "ist_deleted", nullable = false)
+    private boolean isDeleted = false;
+
 }
