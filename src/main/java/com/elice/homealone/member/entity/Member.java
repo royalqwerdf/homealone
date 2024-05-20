746d259fddf951a2b27ac9787ebbf1575a5a0f84
unknown <yunah9119@gmail.com>
2024-05-20 17:07:59 +0900

    feat: room, talk entity

--- src/main/java/com/elice/homealone/member/entity/Member.java
+++ src/main/java/com/elice/homealone/member/entity/Member.java
@@ -1,4 +1,41 @@
 package com.elice.homealone.member.entity;
 
-public class Member {
+import com.elice.homealone.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "member")
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name", length = 15, nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone")
+    private String phone;
+
+    //enum 사용?
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "ist_deleted", nullable = false)
+    private boolean isDeleted = false;
+
 }
