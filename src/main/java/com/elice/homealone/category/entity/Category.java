be2b6db3ea1f049dbdff9e678e3e6d7aa2ee95a5
unknown <yunah9119@gmail.com>
2024-05-20 17:04:04 +0900

    pull dev

--- src/main/java/com/elice/homealone/category/entity/Category.java
+++ src/main/java/com/elice/homealone/category/entity/Category.java
@@ -1,4 +1,13 @@
 package com.elice.homealone.category.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
---
+++ src/main/java/com/elice/homealone/talk/dto/TalkSumaryDto.java
@@ -0,0 +1,15 @@
+package com.elice.homealone.talk.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TalkSumaryDto {
+    private Long id;
+    private String title;
+    private String memberNickname;
+    private Integer commentCount;
+}
