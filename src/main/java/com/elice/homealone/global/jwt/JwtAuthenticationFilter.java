136cdb6161196ad71b05cc329513c33a95a50100
sumin <166582952+royalqwerdf@users.noreply.github.com>
2024-05-26 07:20:59 +0900

    주석 제거

--- src/main/java/com/elice/homealone/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/elice/homealone/global/jwt/JwtAuthenticationFilter.java
@@ -1,39 +1,39 @@
-//package com.elice.homealone.global.jwt;
-//
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import java.io.IOException;
-//
-//
-//public class JwtAuthenticationFilter extends OncePerRequestFilter {
-//
-//    private final JwtTokenProvider jwtTokenProvider;
-////    private final UserDetailsService userDetailsService;
-//
-//    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
-//        this.jwtTokenProvider = jwtTokenProvider;
-////        this.userDetailsService = userDetailsService;
-//    }
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-//            throws ServletException, IOException {
-//        String token = jwtTokenProvider.resolveToken(request);
-//        //TODO userDetail, userDetailService 아직 안만듬..
-////        if (token != null && jwtTokenProvider.validateToken(token)) {
-////            String username = jwtTokenProvider.getUsername(token);
-////            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-////            if (userDetails != null) {
-////                JwtAuthenticationToken authentication = new JwtAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-////                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-////                SecurityContextHolder.getContext().setAuthentication(authentication);
-////            }
-////        }
-//        filterChain.doFilter(request, response);
-//    }
-//}
-//
+package com.elice.homealone.global.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+//    private final UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+//        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
+        //TODO userDetail, userDetailService 아직 안만듬..
+//        if (token != null && jwtTokenProvider.validateToken(token)) {
+//            String username = jwtTokenProvider.getUsername(token);
+//            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+//            if (userDetails != null) {
+//                JwtAuthenticationToken authentication = new JwtAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+//                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//                SecurityContextHolder.getContext().setAuthentication(authentication);
+//            }
+//        }
+        filterChain.doFilter(request, response);
+    }
+}
+
--- src/main/java/com/elice/homealone/member/service/AuthService.java
+++ src/main/java/com/elice/homealone/member/service/AuthService.java
@@ -1,67 +1,67 @@
-//package com.elice.homealone.member.service;
-//
-//
-//import com.elice.homealone.global.jwt.JwtTokenProvider;
-//import com.elice.homealone.member.dto.MemberDto;
-//import com.elice.homealone.member.dto.request.LoginRequestDTO;
-//import com.elice.homealone.member.dto.request.SignupRequestDTO;
-//import com.elice.homealone.member.dto.response.LoginResponseDTO;
-//import com.elice.homealone.member.entity.Member;
-//import com.elice.homealone.member.repository.MemberRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class AuthService {
-//
-//    private final MemberRepository memberRepository;
-//    private final MemberService memberService;
-//    private final JwtTokenProvider jwtTokenProvider;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    /**
-//     * 회원 가입
-//     */
-//    public MemberDto signUp(SignupRequestDTO signupRequestDTO){
-//        String password = passwordEncoder.encode(signupRequestDTO.getPassword());
-//        Member savedMember = signupRequestDTO.toEntity();
-//        savedMember.setPassword(password);
-//        return memberRepository.save(savedMember).toDto();
-//    }
-//
-//    /**
-//     * 로그인
-//     */
-//    public LoginResponseDTO login(LoginRequestDTO loginRequestDTO) {
-//        // 회원 검증
-//        Member findMember = memberService.findByEmail(loginRequestDTO.getEmail());
-//        // 비밀번호 검증
-//        if (passwordEncoder.matches(loginRequestDTO.getPassword(), findMember.getPassword())) {
-//            String token = jwtTokenProvider.createToken(findMember.getEmail());
-//            LoginResponseDTO response = new LoginResponseDTO();
-//            response.setToken(token);
-//            response.setMessage("로그인이 성공했습니다.");
-//            return response;
-//        } else{
-//            throw new RuntimeException("잘못된 비밀번호입니다.");
-//        }
-//    }
-//
-//
-//    /**
-//     * Token으로 회원정보를 찾아서 반환
-//     */
-//
-////    public MemberDto findMemberByToken
-//
-//    /**
-//     * 회원 탈퇴
-//     */
-//    public void withdrawal(MemberDto memberDto) {
-//        Member findedMember = memberService.findByEmail(memberDto.getEmail());
-//        memberRepository.delete(findedMember);
-//    }
-//}
-//
+package com.elice.homealone.member.service;
+
+
+import com.elice.homealone.global.jwt.JwtTokenProvider;
+import com.elice.homealone.member.dto.MemberDto;
+import com.elice.homealone.member.dto.request.LoginRequestDTO;
+import com.elice.homealone.member.dto.request.SignupRequestDTO;
+import com.elice.homealone.member.dto.response.LoginResponseDTO;
+import com.elice.homealone.member.entity.Member;
+import com.elice.homealone.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final MemberRepository memberRepository;
+    private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * 회원 가입
+     */
+    public MemberDto signUp(SignupRequestDTO signupRequestDTO){
+        String password = passwordEncoder.encode(signupRequestDTO.getPassword());
+        Member savedMember = signupRequestDTO.toEntity();
+        savedMember.setPassword(password);
+        return memberRepository.save(savedMember).toDto();
+    }
+
+    /**
+     * 로그인
+     */
+    public LoginResponseDTO login(LoginRequestDTO loginRequestDTO) {
+        // 회원 검증
+        Member findMember = memberService.findByEmail(loginRequestDTO.getEmail());
+        // 비밀번호 검증
+        if (passwordEncoder.matches(loginRequestDTO.getPassword(), findMember.getPassword())) {
+            String token = jwtTokenProvider.createToken(findMember.getEmail());
+            LoginResponseDTO response = new LoginResponseDTO();
+            response.setToken(token);
+            response.setMessage("로그인이 성공했습니다.");
+            return response;
+        } else{
+            throw new RuntimeException("잘못된 비밀번호입니다.");
+        }
+    }
+
+
+    /**
+     * Token으로 회원정보를 찾아서 반환
+     */
+
+//    public MemberDto findMemberByToken
+
+    /**
+     * 회원 탈퇴
+     */
+    public void withdrawal(MemberDto memberDto) {
+        Member findedMember = memberService.findByEmail(memberDto.getEmail());
+        memberRepository.delete(findedMember);
+    }
+}
+
