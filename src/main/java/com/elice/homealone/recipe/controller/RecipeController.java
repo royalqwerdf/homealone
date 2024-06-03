acd13d90fbe61736a88a97b7b74d34d10b7d5ad7
621 <ljh7a21@gmail.com>
2024-06-03 14:39:31 +0900

    Apply .gitignore

--- src/main/java/com/elice/homealone/recipe/controller/RecipeController.java
+++ src/main/java/com/elice/homealone/recipe/controller/RecipeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/recipes")
+@RequestMapping("/api/recipes")
 @RequiredArgsConstructor
 public class RecipeController {
 
