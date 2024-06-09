6fea98342ffbc3dd04e29ae2cd5f2654c5cda4c0
621 <ljh7a21@gmail.com>
2024-06-09 23:19:04 +0900

    fix:git ignore

--- src/main/java/com/elice/homealone/post/sevice/PostService.java
+++ src/main/java/com/elice/homealone/post/sevice/PostService.java
@@ -2,9 +2,16 @@ package com.elice.homealone.post.sevice;
 
 import com.elice.homealone.global.exception.ErrorCode;
 import com.elice.homealone.global.exception.HomealoneException;
+import com.elice.homealone.post.dto.PostResPageDto;
 import com.elice.homealone.post.entity.Post;
 import com.elice.homealone.post.repository.PostRepository;
 
+import com.elice.homealone.recipe.entity.Recipe;
+import com.elice.homealone.recipe.service.RecipeService;
+import com.elice.homealone.room.service.RoomService;
+import com.elice.homealone.talk.Service.TalkService;
+import java.awt.print.Pageable;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,8 +23,11 @@ public class PostService {
 
     private final PostRepository postRepository;
 
-    public Post findById(Long id) {
+    private final RecipeService recipeService;
+    private final RoomService roomService;
+    private final TalkService talkService;
 
+    public Post findById(Long id) {
         Post post = postRepository.findById(id).orElseThrow(() -> new HomealoneException(ErrorCode.POST_NOT_FOUND));
         return post;
     }
