5481578800cbafa33968416d5cdd22b6983c616d
Lazy Cat <mylazysundaycat@gmail.com>
2024-06-09 22:24:07 +0900

    Refactor: Member address->firest_address,seconde_address로 분리

--- src/main/java/com/elice/homealone/member/dto/request/SignupRequestDto.java
+++ src/main/java/com/elice/homealone/member/dto/request/SignupRequestDto.java
@@ -19,14 +19,16 @@ public class SignupRequestDto {
     @NotNull
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$")
     private String email;
-    private String address;
+    private String firstAddress;
+    private String secondAddress;
     private String password;
     public Member toEntity() {
         Member member = new Member();
         member.setName(this.name);
         member.setBirth(this.birth);
         member.setEmail(this.email);
-        member.setAddress(this.address);
+        member.setFirstAddress(this.firstAddress);
+        member.setSecondAddress(this.secondAddress);
         member.setPassword(this.password);
         return member;
     }
