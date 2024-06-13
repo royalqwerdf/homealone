50d6ee40fe7d30040b78b1c20924ec4d2df6a500
621 <ljh7a21@gmail.com>
2024-06-13 22:38:22 +0900

    Feat : recipe trands

--- src/main/java/com/elice/homealone/module/post/repository/PostRepository.java
+++ src/main/java/com/elice/homealone/module/post/repository/PostRepository.java
@@ -1,10 +1,22 @@
 package com.elice.homealone.module.post.repository;
 
 import com.elice.homealone.module.post.entity.Post;
+import java.time.LocalDateTime;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     int countById(Long id);
     Optional<Post> findById(Long id);
+
+    @Query("SELECT p FROM Post p " +
+        "LEFT JOIN p.likes l " +
+        "WHERE p.type = :type AND p.createdAt >= :oneWeekAgo " +
+        "GROUP BY p.id " +
+        "ORDER BY COUNT(l) DESC, p.createdAt DESC")
+    Page<Post> findRecipesByLikesInLastWeek(@Param("type") Post.Type type, @Param("oneWeekAgo") LocalDateTime oneWeekAgo, Pageable pageable);
 }
