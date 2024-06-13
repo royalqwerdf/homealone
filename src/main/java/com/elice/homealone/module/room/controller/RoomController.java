0251e11401c94110799184fdd225d29e716152af
621 <ljh7a21@gmail.com>
2024-06-14 01:33:40 +0900

    fix : error

--- src/main/java/com/elice/homealone/module/room/controller/RoomController.java
+++ src/main/java/com/elice/homealone/module/room/controller/RoomController.java
@@ -72,7 +72,7 @@ public class RoomController {
     }
     @Operation(summary = "방자랑 게시글 인기글 조회")
     @GetMapping("/view")
-    public ResponseEntity<Page<RoomResponseDTO>> findTopRoomByView(@PageableDefault(size = 6) Pageable pageable){
+    public ResponseEntity<Page<RoomResponseDTO>> findTopRoomByView(@PageableDefault(size = 4) Pageable pageable){
         Page<RoomResponseDTO> topRoomByView = roomService.findTopRoomByView(pageable);
         return ResponseEntity.ok(topRoomByView);
     }
